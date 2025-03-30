1be10078491f52c7fc4818409ec7dc9ac751a7ce
jamie scott <mrjamiescott1995@gmail.com>
2025-03-30 04:14:16 +0100

    simple Profiler

---
+++ Headers/System/Profiler.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <chrono>
+#ifndef SYSTEM_PROFILER_H
+#define SYSTEM_PROFILER_H
+
+#if defined(SYSTEM_PROFILER_ENABLED)
+#define SYSTEM_PROFILER_BEGIN_FRAME() System::Profiler::BeginFrame();
+#define SYSTEM_PROFILER_END_FRAME() System::Profiler::EndFrame();
+#define SYSTEM_PROFILER_BEGIN_SAMPLE(x) System::Profiler::BeginSample(x);
+#define SYSTEM_PROFILER_END_SAMPLE() System::Profiler::EndSample() ;
+#else
+#define SYSTEM_PROFILER_BEGIN_FRAME()
+#define SYSTEM_PROFILER_END_FRAME()
+#define SYSTEM_PROFILER_BEGIN_SAMPLE(x)
+#define SYSTEM_PROFILER_END_SAMPLE() 
+#endif
+
+ 
+
+namespace System {
+    class Profiler{
+
+    public:
+        static void BeginFrame();
+        static void EndFrame();
+        static void BeginSample(const char* name);
+        static void EndSample();
+
+    private:
+        struct Sample {
+            const char* name;
+            std::chrono::high_resolution_clock::time_point start;
+            std::chrono::high_resolution_clock::time_point end;
+        };
+
+        static const int maxSamples = 31;
+        static int sampleIndex;
+        static std::chrono::high_resolution_clock::time_point currentFrameStart;
+        static std::array<Sample, maxSamples> samples;
+    };
+}
+
+#endif // PROFILER_H
---
+++ Sources/System/Profiler.cpp
@@ -0,0 +1,56 @@
+#include <System/Profiler.h>
+//#include <chrono>
+#include <array>
+#include <string>
+#include <iostream>
+namespace System{
+    // Definition of static member variables
+    int Profiler::sampleIndex = 0;
+    std::chrono::high_resolution_clock::time_point Profiler::currentFrameStart;
+    std::array<Profiler::Sample, Profiler::maxSamples> Profiler::samples;
+
+ void Profiler::BeginFrame() {
+    currentFrameStart = std::chrono::high_resolution_clock::now();
+    sampleIndex = 0; // Reset the sample index at the beginning of each frame
+}
+ void Profiler::EndFrame() {
+    std::cout << std::dec;
+    auto frameEnd = std::chrono::high_resolution_clock::now();
+    auto frameDuration = std::chrono::duration_cast<std::chrono::microseconds>(frameEnd - currentFrameStart).count();
+    std::cout << "Frame Time: " << frameDuration << " microseconds" << std::endl;
+
+    // Print all samples at the end of the frame
+    for (int i = 0; i < sampleIndex; ++i) {
+        auto sampleDuration = std::chrono::duration_cast<std::chrono::microseconds>(samples[i].end - samples[i].start).count();
+        std::cout << "Sample \"" << samples[i].name << "\" Time: " << sampleDuration << " microseconds" << std::endl;
+    }
+}
+ void Profiler::BeginSample(const char* name) {
+    if (sampleIndex < maxSamples) {
+        samples[sampleIndex].name = name;
+        samples[sampleIndex].start = std::chrono::high_resolution_clock::now();
+        sampleIndex++;
+    }
+    else {
+        std::cerr << "Maximum sample count reached. Ensure to keep within 31 samples per frame." << std::endl;
+    }
+}
+ void Profiler::EndSample() {
+    if (sampleIndex > 0) {
+        auto sampleEnd = std::chrono::high_resolution_clock::now();
+        samples[sampleIndex - 1].end = sampleEnd;
+    }
+    else {
+        std::cerr << "No sample to end. Ensure BeginSample() was called." << std::endl;
+    }
+}
+
+
+
+
+
+
+
+
+
+}
