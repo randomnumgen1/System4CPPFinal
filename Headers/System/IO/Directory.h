f4b940ebdc3ba12460f2008f88e51ff426faf13e
jamie scott <mrjamiescott1995@gmail.com>
2025-04-17 20:10:23 +0100

    Create Directory.h

---
+++ Headers/System/IO/Directory.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <string>
+namespace System::IO{
+	class Directory{
+		static void CreateDirectory(std::string path){
+			
+		}
+		static void CreateSymbolicLink(std::string path, std::string pathToTarget){
+			
+		}
+		static void Delete(std::string path){
+			
+		}
+		static bool Exists(std::string path){
+			
+		}
+		static void Move(std::string sourceDirName, std::string destDirName){
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+	};
+}
