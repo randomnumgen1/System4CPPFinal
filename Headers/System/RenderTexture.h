8c30087c467a7aa8acd23c9d0fd9db236afd13bd
jamie scott <mrjamiescott1995@gmail.com>
2025-05-03 06:24:04 +0100

    Create RenderTexture.h

---
+++ Headers/System/RenderTexture.h
@@ -0,0 +1,20 @@
+#ifndef _SYSTEM_RENDERTEXTURE_H
+#define _SYSTEM_RENDERTEXTURE_H 
+#include <stdint.h>
+
+typedef int* IntPtr_t;
+namespace System {
+    struct RenderTexture {
+        RenderTexture() {
+        
+        }
+        intptr_t GetNativeTexturePtr() {
+        
+        }
+        int GetWidth() {}
+        int GetHeight() {}
+
+    
+    };
+}
+#endif
