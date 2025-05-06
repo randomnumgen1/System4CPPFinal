44c58e2299fe5df4dc73214c83a9f8c9397b8e9f
jamie scott <mrjamiescott1995@gmail.com>
2025-05-06 20:37:09 +0100

    Create Physics.h

---
+++ Headers/System/Physics/Physics.h
@@ -0,0 +1,15 @@
+#ifndef _SYSTEM_PHYSICS_H
+#define _SYSTEM_PHYSICS_H
+
+#include <System/Vector3.hpp>
+#include <cstdint>
+
+namespace System{
+    class Physics{
+    public:
+        static bool Raycast(Vector3 origin, Vector3 direction, float maxDistance, uint32_t layerMask){
+           
+        }
+    };
+}
+#endif
