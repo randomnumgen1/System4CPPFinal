61326451790d9ff3766a88f0d01aa95bdac575ba
jamie scott <mrjamiescott1995@gmail.com>
2025-05-07 19:17:38 +0100

    RenderTexture: generate texture

--- Headers/System/RenderTexture.h
+++ Headers/System/RenderTexture.h
@@ -1,18 +1,33 @@
 #ifndef _SYSTEM_RENDERTEXTURE_H
-#define _SYSTEM_RENDERTEXTURE_H 
-#include <stdint.h>
+#define _SYSTEM_RENDERTEXTURE_H
 
+#include <stdint.h>
+#include <System/Graphics/GraphicsHelpers.h>
 typedef int* IntPtr_t;
 namespace System {
     struct RenderTexture {
+        GLuint renderedTexture;
         RenderTexture() {
-        
+           
+            System::Graphics::GL::gl_glGenTextures(1, &renderedTexture);
+            System::Graphics::GL::gl_glBindTexture(GL_TEXTURE_2D, renderedTexture);
+            System::Graphics::GL::gl_glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 1024, 768, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+            System::Graphics::GL::gl_glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+            System::Graphics::GL::gl_glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
         }
-        intptr_t GetNativeTexturePtr() {
-        
+        bool isCreated() const {
+            return renderedTexture != 0;
         }
+        intptr_t GetNativeTexturePtr() const{
+            return  renderedTexture ;
+        }
+
         int GetWidth() {}
         int GetHeight() {}
+        ~RenderTexture() {
+            System::Graphics::GL::gl_glDeleteTextures(1, &renderedTexture);
+        }
 
     
     };
