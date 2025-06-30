dde58f9e28b858117684792b52e59186cd561bd8
jamie scott <mrjamiescott1995@gmail.com>
2025-06-30 19:27:35 +0100

    TrueTypeFont: link to spec

--- Headers/System/Internal/TrueTypeFont.h
+++ Headers/System/Internal/TrueTypeFont.h
@@ -1,13 +1,13 @@
 #ifndef SYSTEM_INTERNAL_TRUETYPEFONT_H
 #define SYSTEM_INTERNAL_TRUETYPEFONT_H
-
+//file format spec: https://learn.microsoft.com/en-us/typography/opentype/spec/glyf
 #include <stdexcept>
 
 namespace System::Internal{
 	class TrueTypeFont{
 		private:
 			constexpr uint32_t TTF_CHECKSUM_MAGIC = 0xB1B0AFBA;
-			
+			//static vector<font_t> fonts;
 			struct TableDirectory_t{
 				uint32_t scaler_type;
 				uint16_t numTables;
@@ -60,11 +60,42 @@ namespace System::Internal{
 			}
 			//maximum profile
 			struct maxp_t{
-				
+  version: reader.getFixed(),
+				uint16_t numGlyphs;
+				uint16_t maxPoints;
+				uint16_t maxContours;
+				uint16_t maxCompositePoints;
+				uint16_t maxCompositeContours;
+				uint16_t maxZones;
+				uint16_t maxTwilightPoints;
+				uint16_t maxStorage;
+				uint16_t maxFunctionDefs;
+				uint16_t maxInstructionDefs;
+				uint16_t maxStackElements;
+				uint16_t maxSizeOfInstructions;
+				uint16_t maxComponentElements;
+				uint16_t maxComponentDepth;			
 			}
 			//horizontal header
 			struct hhea_t{
-				
+				uint16_t majorVersion;
+				uint16_t minorVersion;
+				FWORD ascender;
+				FWORD descender;
+				FWORD lineGap;
+				UFWORD advanceWidthMax;
+				FWORD minLeftSideBearing;
+				FWORD minRightSideBearing;
+				FWORD xMaxExtent;
+				int16_t	caretSlopeRise;
+				int16_t	caretSlopeRun;
+				int16_t	caretOffset;
+				int16_t	reserved1;
+				int16_t	reserved2;
+				int16_t	reserved3;
+				int16_t	reserved4;
+				int16_t	metricDataFormat;
+				uint16_t numberOfHMetrics;
 			}
 			//horizontal metrics
 			struct hmtx_t{
@@ -76,17 +107,33 @@ namespace System::Internal{
 			}
 			//glyph outline
 			struct glyf_t{
+				int16	numberOfContours;
+				int16	xMin
+				int16	yMin
+				int16	xMax
+				int16	yMax
 				
 			}
 			//character code mapping
 			struct cmap_t{
 				
 			}
+			static bool ends_with(const std::string& str, const std::string& suffix){
+				return str.size() >= suffix.size() && str.compare(str.size()-suffix.size(), suffix.size(), suffix) == 0;
+			}
+			static bool starts_with(const std::string& str, const std::string& prefix){
+				return str.size() >= prefix.size() && str.compare(0, prefix.size(), prefix) == 0;
+			} 
+
 		public:
 			void LoadFont(std::string filepath){
+				if (!ends_with(filepath, ".ttf") && !ends_with(filepath, ".TTF")){
+					throw std::invalid_argument( "TrueTypeFont [Error] Filename not .ttf");
+					return;
+				}
 				std::ifstream file(filepath, std::ios::binary);
 				if (!file) {
-					throw std::invalid_argument( "TrueTypeFont File Error" );
+					throw std::invalid_argument( "TrueTypeFont [Error] Cannot open file");
 					return;
 				}
 				TableDirectory_t offsetHeader;
