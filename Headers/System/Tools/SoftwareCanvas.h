73425a28b2683fa4cc7435f7ae60c616554659ec
jamie scott <mrjamiescott1995@gmail.com>
2025-07-17 16:36:22 +0100

    SoftwareCanvas save as bitmap

--- Headers/System/Tools/SoftwareCanvas.h
+++ Headers/System/Tools/SoftwareCanvas.h
@@ -178,6 +178,61 @@ namespace System::Tools{
 			}
 		public:
 			SoftwareCanvas(int w, int h);
+			//
+			void SaveAsBitmap(const std::string& filename){
+				std::ofstream file(filename, std::ios::binary);
+				if (!file) {
+					return false;
+				}
+
+				// BMP Header
+				const int fileSize = 54 + m_width * m_height * 4;
+				const int reserved = 0;
+				const int dataOffset = 54;
+
+				file.put('B');
+				file.put('M');
+				file.write(reinterpret_cast<const char*>(&fileSize), 4);
+				file.write(reinterpret_cast<const char*>(&reserved), 4);
+				file.write(reinterpret_cast<const char*>(&dataOffset), 4);
+
+				// DIB Header
+				const int dibHeaderSize = 40;
+				const int planes = 1;
+				const int bpp = 32;
+				const int compression = 0;
+				const int imageSize = m_width * m_height * 4;
+				const int xPelsPerMeter = 2835; // 72 DPI
+				const int yPelsPerMeter = 2835; // 72 DPI
+				const int clrUsed = 0;
+				const int clrImportant = 0;
+
+				file.write(reinterpret_cast<const char*>(&dibHeaderSize), 4);
+				file.write(reinterpret_cast<const char*>(&m_width), 4);
+				file.write(reinterpret_cast<const char*>(&m_height), 4);
+				file.write(reinterpret_cast<const char*>(&planes), 2);
+				file.write(reinterpret_cast<const char*>(&bpp), 2);
+				file.write(reinterpret_cast<const char*>(&compression), 4);
+				file.write(reinterpret_cast<const char*>(&imageSize), 4);
+				file.write(reinterpret_cast<const char*>(&xPelsPerMeter), 4);
+				file.write(reinterpret_cast<const char*>(&yPelsPerMeter), 4);
+				file.write(reinterpret_cast<const char*>(&clrUsed), 4);
+				file.write(reinterpret_cast<const char*>(&clrImportant), 4);
+
+				// Pixel Data
+				for (int y = m_height - 1; y >= 0; --y) {
+					for (int x = 0; x < m_width; ++x) {
+						int idx = (y * m_width + x) * 4;
+						// BMP expects BGR order
+						file.put(m_pixels[idx + 2]);
+						file.put(m_pixels[idx + 1]);
+						file.put(m_pixels[idx + 0]);
+						file.put(m_pixels[idx + 3]);
+					}
+				}
+
+				return true;
+			}
 			// save/restore:
 			void save();
 			void restore();
