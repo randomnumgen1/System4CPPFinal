7305b069db0c24e36b83b8741349df3cb3172b43
jamie scott <mrjamiescott1995@gmail.com>
2025-07-18 11:24:19 +0100

    Matrix3x3: consistency

--- Headers/System/Matrix3x3.hpp
+++ Headers/System/Matrix3x3.hpp
@@ -55,13 +55,13 @@ namespace System {
 
 			Matrix3x3 inv;
 			inv.m[0] = (m[4] * m[8] - m[7] * m[5]) * invDet;
-			inv.m[3] = (m[6] * m[5] - m[3] * m[8]) * invDet;
-			inv.m[6] = (m[3] * m[7] - m[6] * m[4]) * invDet;
-			inv.m[1] = (m[7] * m[2] - m[1] * m[8]) * invDet;
-			inv.m[4] = (m[0] * m[8] - m[6] * m[2]) * invDet;
-			inv.m[7] = (m[6] * m[1] - m[0] * m[7]) * invDet;
-			inv.m[2] = (m[1] * m[5] - m[4] * m[2]) * invDet;
-			inv.m[5] = (m[3] * m[2] - m[0] * m[5]) * invDet;
+			inv.m[3] = (m[2] * m[7] - m[1] * m[8]) * invDet;
+			inv.m[6] = (m[1] * m[5] - m[2] * m[4]) * invDet;
+			inv.m[1] = (m[5] * m[6] - m[3] * m[8]) * invDet;
+			inv.m[4] = (m[0] * m[8] - m[2] * m[6]) * invDet;
+			inv.m[7] = (m[2] * m[3] - m[0] * m[5]) * invDet;
+			inv.m[2] = (m[3] * m[7] - m[5] * m[6]) * invDet;
+			inv.m[5] = (m[1] * m[6] - m[0] * m[7]) * invDet;
 			inv.m[8] = (m[0] * m[4] - m[3] * m[1]) * invDet;
 
 			return inv;
@@ -75,22 +75,22 @@ namespace System {
 			m[1] = 0.f; m[4] = 1.f; m[7] = 0.f;
 			m[2] = 0.f; m[5] = 0.f; m[8] = 1.f;			
 		}
-		Matrix3x3(float m0, float m1, float m2, float m3, float m4, float m5, float m6, float m7, float m8) {
-			m[0] = m0; m[1] = m1; m[2] = m2;
-			m[3] = m3; m[4] = m4; m[5] = m5;
-			m[6] = m6; m[7] = m7; m[8] = m8;
+		Matrix3x3(float m00, float m10, float m20, float m01, float m11, float m21, float m02, float m12, float m22) {
+			m[0] = m00; m[1] = m10; m[2] = m20;
+			m[3] = m01; m[4] = m11; m[5] = m21;
+			m[6] = m02; m[7] = m12; m[8] = m22;
 		}
 		static Matrix3x3 Translate(float tx, float ty) {
 			return Matrix3x3(
-				1.f, 0.f, 0.f,
-				0.f, 1.f, 0.f,
-				tx , ty , 1.f
+				1.f, 0.f, tx,
+				0.f, 1.f, ty,
+				0.f, 0.f, 1.f
 			);
 		}
 		static Matrix3x3 Scale(float sx, float sy) {
 			return Matrix3x3(
-				sx , 0.f, 0.f,
-				0.f, sy , 0.f,
+				sx, 0.f, 0.f,
+				0.f, sy, 0.f,
 				0.f, 0.f, 1.f
 			);
 		}
@@ -98,9 +98,9 @@ namespace System {
 			float c = std::cos(radians);
 			float s = std::sin(radians);
 			return Matrix3x3(
-				 c ,  s , 0.f,
-				-s ,  c , 0.f,
-				 0.f, 0.f, 1.f
+				c, -s, 0.f,
+				s, c, 0.f,
+				0.f, 0.f, 1.f
 			);
 		}
 		
