5beed229d58566d60f1dee3c4ee5ab8aaf09b249
jamie scott <mrjamiescott1995@gmail.com>
2025-07-21 12:26:21 +0100

    Software Canvas Tests

---
+++ Tests/SoftwareCanvasTests.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+#include "System/Tools/SoftwareCanvas.h"
+#include <fstream>
+#include <vector>
+#include <chrono>
+
+TEST(SoftwareCanvasTests, Constructor) {
+    System::Tools::SoftwareCanvas canvas(100, 100);
+    SUCCEED();
+}
+TEST(SoftwareCanvasTests, ConstructorTiming) {
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 1000; ++i) {
+        System::Tools::SoftwareCanvas canvas(100, 100);
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> elapsed = end - start;
+    std::cout << "Constructor timing (1000 iterations): " << elapsed.count() << " ms" << std::endl;
+    SUCCEED();
+}
+TEST(SoftwareCanvasTests, TimingTest) {
+    System::Tools::SoftwareCanvas canvas(10, 10);
+	auto start = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < 1000; ++i) {
+		canvas.rect(2, 2, 6, 6);
+		canvas.clip();
+		canvas.setFillStyle(255, 0, 0, 255);
+		canvas.rect(0, 0, 10, 10);
+		canvas.fill();
+	}
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> elapsed = end - start;
+    std::cout << "Constructor timing (1000 iterations): " << elapsed.count() << " ms" << std::endl;
+    SUCCEED();
+}
