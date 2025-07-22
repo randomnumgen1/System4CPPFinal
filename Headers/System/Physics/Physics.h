ee3f9c75c004132aa2526c428c13f60b04ea2157
jamie scott <mrjamiescott1995@gmail.com>
2025-07-22 19:08:16 +0100

    Physics: raycast

--- Headers/System/Physics/Physics.h
+++ Headers/System/Physics/Physics.h
@@ -3,13 +3,33 @@
 
 #include <System/Vector3.hpp>
 #include <cstdint>
+#include <System/Mathf.hpp>
 
 namespace System{
+    struct Ray {
+    public:
+        Vector3 origin;
+        Vector3 direction;
+        Ray(Vector3 origin, Vector3 direction){
+            this->origin = origin;
+            this->direction = direction;
+        }
+    };
     class Physics{
     public:
-        static bool Raycast(Vector3 origin, Vector3 direction, float maxDistance, uint32_t layerMask){
+        static bool Raycast(Vector3 origin, Vector3 direction, float maxDistance = System::Mathf::Infinity, uint32_t layerMask = -1){
            
         }
+
+
+
+
+
+
+
+
+
+
     };
 }
 #endif
