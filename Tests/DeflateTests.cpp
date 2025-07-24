82aca7d65187e39f3a7f04fe6cb851b7e8cc02b8
jamie scott <mrjamiescott1995@gmail.com>
2025-07-24 23:30:53 +0100

    Update DeflateTests.cpp

--- Tests/DeflateTests.cpp
+++ Tests/DeflateTests.cpp
@@ -2,16 +2,11 @@
 #include "System/IO/Compression/Deflate.hpp"
 #include "System/IO/File.h"
 
-//The first block is a fixed huffman block, and the second is a stored block. The first block is not the last block, and the second block is the last block. 
+//The first block is a fixed huffman block, and the second is a stored block. The first block is not the last block, and the second block is the last block.
 TEST(DeflateTests, DeflateDecompressFile) {
-	System::IO::Compression::Deflate deflate;
+    System::IO::Compression::Deflate deflate;
     std::vector<uint8_t> decompressed_data = System::IO::Compression::Deflate::Decompress(System::IO::File::ReadAllBytes("compressed.deflate"));
-	if (std::string(decompressed_data.begin(), decompressed_data.end()) == "Hello, World!") {
-		SUCCEED();
-	}else{
-		FAIL();
-		
-	}
-	
+    std::string decompressed_string(decompressed_data.begin(), decompressed_data.end());
+    ASSERT_EQ(decompressed_string, "Hello, World!");
 }
  
