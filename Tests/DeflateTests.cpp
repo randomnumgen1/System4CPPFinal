7ece7f249b2e0b18f43c327535d24bf6c574102f
jamie scott <mrjamiescott1995@gmail.com>
2025-07-24 20:14:23 +0100

    Update DeflateTests.cpp

---
+++ Tests/DeflateTests.cpp
@@ -0,0 +1,21 @@
+
+
+TEST(DeflateTests, DeflateDecompression) {
+	
+    std::vector<uint8_t> compressed_data = {0x7A, 0xBF, 0x7B, 0xBF, 0x47, 0x6A, 0x4E, 0x4E, 0xBE, 0x8E, 0x42, 0x78, 0x7E, 0x51, 0x4E, 0x8A, 0x22, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x03, 0x00};
+
+    std::vector<uint8_t> decompressed_data = System::IO::Compression::Deflate::Decompress(compressed_data);
+	
+	
+	if (std::string(decompressed_data.begin(), decompressed_data.end()) == "Hello, World!") {
+		SUCCEED();
+	}else{
+		
+		
+	}
+	
+	
+	
+
+}
+
