3883412b6b3b3a530a9ad1b85ff24c8637581cd4
jamie scott <mrjamiescott1995@gmail.com>
2025-07-24 17:21:13 +0100

    Create LZSS.hpp

---
+++ Headers/System/IO/Compression/LZSS.hpp
@@ -0,0 +1,18 @@
+#ifndef _SYSTEM_IO_COMPRESSION_LZSS_H
+#define _SYSTEM_IO_COMPRESSION_LZSS_H
+
+#include <vector>
+namespace System {
+    namespace IO {
+        namespace Compression {
+            class LZSS {
+                void Compress(const std::vector<uint8_t>& data) {
+                
+                }
+
+
+            };
+        }
+    }
+}
+#endif
