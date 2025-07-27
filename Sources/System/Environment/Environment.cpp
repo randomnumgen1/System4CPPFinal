e2e264426b98a6f68eaa96274e0506415c8a13d2
jamie scott <mrjamiescott1995@gmail.com>
2025-07-27 20:28:53 +0100

    Update Environment.cpp

--- Sources/System/Environment/Environment.cpp
+++ Sources/System/Environment/Environment.cpp
@@ -42,7 +42,6 @@ namespace System {
 #elif (defined(__GNUC__) || defined(__clang__)) && !defined(__aarch64__)
             __get_cpuid(0x80000002 + i, &regs[0], &regs[1], &regs[2], &regs[3]);
 #else
-#error "unsupported arch"
 #endif
             // Append the brand parts
             brand += std::string((const char*)&regs[0], 4);
