39cc4f89ab822f31997f3bb305b87feead119e4c
jamie scott <mrjamiescott1995@gmail.com>
2025-08-01 20:20:15 +0100

    g

--- Tests/Compression/ZLibTests.cpp
+++ Tests/Compression/ZLibTests.cpp
@@ -2,11 +2,13 @@
 #include "System/IO/Compression/ZLib.hpp"
 #include "System/IO/File.h"
 
-TEST(ZLibTests, ZLibDecompressFile) {
-    // Assuming the test will be run from a subdirectory in Tests, like Tests/Compression
+
+//decompresses the data generated from generate_zlib1.py
+TEST(ZLibTests, ZLibDecompressSimpleFile) {
     std::vector<uint8_t> decompressed_data = System::IO::Compression::ZLib::Decompress(System::IO::File::ReadAllBytes("max_compressed1.zlib"));
     std::string decompressed_string(decompressed_data.begin(), decompressed_data.end());
-	ASSERT_EQ(decompressed_string, std::string("Hello ZLib!"));
+    std::string expected_string(100000, 'A');
+    ASSERT_EQ(decompressed_string, expected_string);
 }
 //decompresses the data generated from generate_zlib2.py
 TEST(ZLibTests, ZLibDecompressMaxCompressedFile) {
