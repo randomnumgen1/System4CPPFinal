c3cf7852d72c6318e0d3488f3a3944d9582caa1c
jamie scott <mrjamiescott1995@gmail.com>
2025-08-01 15:21:37 +0100

    Deflate: optimise copies

--- Headers/System/IO/Compression/Deflate.hpp
+++ Headers/System/IO/Compression/Deflate.hpp
@@ -68,7 +68,29 @@ namespace System {
 						}
 					}
 				}
+				int decode(const uint8_t* data, size_t data_len, int& bit_position) const {
+					int currentNode = 0;
 
+					while (tree[currentNode].Left != -1) {
+						int byte_index = bit_position / 8;
+						if (byte_index >= data_len) {
+							throw std::runtime_error("Error: decode reading past file");
+						}
+						int bit = (data[byte_index] >> (bit_position % 8)) & 1;
+						bit_position++;
+
+						if (bit) {
+							currentNode = tree[currentNode].Right;
+						}
+						else {
+							currentNode = tree[currentNode].Left;
+						}
+						if (currentNode == -1) {
+							throw std::runtime_error("Error: invalid huffman code");
+						}
+					}
+					return tree[currentNode].Symbol;
+				}
 				int decode(const std::vector<uint8_t>& data, int& bit_position) const {
 					int currentNode = 0;
 					int current_byte = data[bit_position / 8];
@@ -133,13 +155,13 @@ namespace System {
 			class Deflate {
 			private:
 				// Constants for Deflate algorithm
-				static const int MAX_LITERALS = 288;
-				static const int MAX_DISTANCES = 32;
-				static const int END_OF_BLOCK = 256;
-				static const int LENGTH_CODES_START = 257;
-				static const int NUM_LENGTH_CODES = 29;
-				static const int NUM_DISTANCE_CODES = 30;
-				static const int CODE_LENGTH_CODES = 19;
+				static constexpr int MAX_LITERALS = 288;
+				static constexpr int MAX_DISTANCES = 32;
+				static constexpr int END_OF_BLOCK = 256;
+				static constexpr int LENGTH_CODES_START = 257;
+				static constexpr int NUM_LENGTH_CODES = 29;
+				static constexpr int NUM_DISTANCE_CODES = 30;
+				static constexpr int CODE_LENGTH_CODES = 19;
 				// Enum for block types in Deflate stream
 				enum BlockType {
 					Stored = 0b00,
@@ -152,6 +174,23 @@ namespace System {
 					Last = 1,
 					More = 0
 				}; 
+				static uint32_t read_bits(const uint8_t* data, size_t data_len, int& bit_position, int count) {
+					uint32_t value = 0;
+					for (int i = 0; i < count; ++i) {
+						int byte_index = (bit_position + i) / 8;
+						int bit_index = (bit_position + i) % 8;
+						if (byte_index < data_len) {
+							if ((data[byte_index] >> bit_index) & 1) {
+								value |= (1 << i);
+							}
+						}
+						else {
+							throw std::runtime_error("Error:  read_bits reading past file");
+						}
+					}
+					bit_position += count;
+					return value;
+				}
 				// Reads a specified number of bits from the data vector
 				static uint32_t read_bits(const std::vector<uint8_t>& data, int& bit_position, int count) {
 					uint32_t value = 0;
@@ -286,18 +325,18 @@ namespace System {
 				/// <param name="result"></param>
 				/// <param name="data"></param>
 				/// <param name="bit_position"></param>
-				static void ReadStoredBlock(std::vector<uint8_t>& result, const std::vector<uint8_t>& data, int& bit_position) {
+				static void ReadStoredBlock(std::vector<uint8_t>& result, const uint8_t* data, size_t data_len, int& bit_position) {
 					// Skip to the next byte boundary
 					bit_position = (bit_position + 7) & ~7;
 					int byte_position = bit_position / 8;
 					// Check if we have enough input data to read the length and negated length
-					if (byte_position + 4 > data.size()) {
+					if (byte_position + 4 > data_len) {
 						throw std::runtime_error("Error: invalid stored block length");
 					}
-					// Read length and negated length
-					uint16_t len = *(uint16_t*)(data.data() + byte_position);
+					// Read length and negated length safely
+					uint16_t len = (uint16_t)data[byte_position] | ((uint16_t)data[byte_position + 1] << 8);
 					byte_position += 2;
-					uint16_t nlen = *(uint16_t*)(data.data() + byte_position);
+					uint16_t nlen = (uint16_t)data[byte_position] | ((uint16_t)data[byte_position + 1] << 8);
 					byte_position += 2;
 					bit_position += 32;
 					// Check if length and negated length are complements
@@ -307,10 +346,10 @@ namespace System {
 					// straight copy data as we are on a byte boundary
 					byte_position = bit_position / 8;
 					// Check if we have enough input data to read the content data
-					if (byte_position + len > data.size()) {
+					if (byte_position + len > data_len) {
 						throw std::runtime_error("Error: invalid stored block length");
 					}
-					result.insert(result.end(), data.begin() + byte_position, data.begin() + byte_position + len);
+					result.insert(result.end(), data + byte_position, data + byte_position + len);
 					bit_position += len * 8;
 				}
 				/// <summary>
@@ -321,7 +360,7 @@ namespace System {
 				/// <param name="result"></param>
 				/// <param name="data"></param>
 				/// <param name="bit_position"></param>
-				static void ReadStaticBlock(std::vector<uint8_t>& result, const std::vector<uint8_t>& data, int& bit_position){
+				static void ReadStaticBlock(std::vector<uint8_t>& result, const uint8_t* data, size_t data_len, int& bit_position) {
 					std::vector<int> literal_lengths(MAX_LITERALS, 0);
 					for (int i = 0; i <= 143; ++i) literal_lengths[i] = 8;
 					for (int i = 144; i <= 255; ++i) literal_lengths[i] = 9;
@@ -339,7 +378,7 @@ namespace System {
 
 
 					while (true) {
-						int symbol = literal_huffman.decode(data, bit_position);
+						int symbol = literal_huffman.decode(data, data_len, bit_position);
 						if (symbol < END_OF_BLOCK) {
 							result.push_back(symbol);
 						}
@@ -347,10 +386,10 @@ namespace System {
 							break;
 						}
 						else {
-							int length = length_starts[symbol - LENGTH_CODES_START] + read_bits(data, bit_position, length_extra_bits[symbol - LENGTH_CODES_START]);
+							int length = length_starts[symbol - LENGTH_CODES_START] + read_bits(data, data_len, bit_position, length_extra_bits[symbol - LENGTH_CODES_START]);
 
-							int distance_symbol = distance_huffman.decode(data, bit_position);
-							int distance = distance_starts[distance_symbol] + read_bits(data, bit_position, distance_extra_bits[distance_symbol]);
+							int distance_symbol = distance_huffman.decode(data, data_len, bit_position);
+							int distance = distance_starts[distance_symbol] + read_bits(data, data_len, bit_position, distance_extra_bits[distance_symbol]);
 
 							int start = result.size() - distance;
 							for (int i = 0; i < length; ++i) {
@@ -367,43 +406,42 @@ namespace System {
 				/// <param name="result"></param>
 				/// <param name="data"></param>
 				/// <param name="bit_position"></param>
-				static void ReadDynamicBlock(std::vector<uint8_t>& result, const std::vector<uint8_t>& data, int& bit_position){
-					uint16_t hlit = read_bits(data, bit_position, 5) + 257;
-					uint16_t hdist = read_bits(data, bit_position, 5) + 1;
-					uint16_t hclen = read_bits(data, bit_position, 4) + 4;
-					std::cout << "hlit: " << hlit << ", hdist: " << hdist << ", hclen: " << hclen << std::endl;
+				static void ReadDynamicBlock(std::vector<uint8_t>& result, const uint8_t* data, size_t data_len, int& bit_position) {
+					uint16_t hlit = read_bits(data, data_len, bit_position, 5) + 257;
+					uint16_t hdist = read_bits(data, data_len, bit_position, 5) + 1;
+					uint16_t hclen = read_bits(data, data_len, bit_position, 4) + 4;
 
 					std::vector<int> code_lengths_lengths(19, 0);
 					int
 						code_lengths_order[19] = { 16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15 };
 
 					for (int i = 0; i < hclen; ++i) {
-						code_lengths_lengths[code_lengths_order[i]] = read_bits(data, bit_position, 3);
+						code_lengths_lengths[code_lengths_order[i]] = read_bits(data, data_len, bit_position, 3);
 					}
 
 					DeflateHuffmanTree code_lengths_huffman(code_lengths_lengths);
 
 					std::vector<int> literal_lengths;
 					while (literal_lengths.size() < hlit + hdist) {
-						int symbol = code_lengths_huffman.decode(data, bit_position);
+						int symbol = code_lengths_huffman.decode(data, data_len, bit_position);
 						if (symbol < 16) {
 							literal_lengths.push_back(symbol);
 						}
 						else if (symbol == 16) {
-							int repeat_count = read_bits(data, bit_position, 2) + 3;
+							int repeat_count = read_bits(data, data_len, bit_position, 2) + 3;
 							int previous_length = literal_lengths.back();
 							for (int i = 0; i < repeat_count; ++i) {
 								literal_lengths.push_back(previous_length);
 							}
 						}
 						else if (symbol == 17) {
-							int repeat_count = read_bits(data, bit_position, 3) + 3;
+							int repeat_count = read_bits(data, data_len, bit_position, 3) + 3;
 							for (int i = 0; i < repeat_count; ++i) {
 								literal_lengths.push_back(0);
 							}
 						}
 						else if (symbol == 18) {
-							int repeat_count = read_bits(data, bit_position, 7) + 11;
+							int repeat_count = read_bits(data, data_len, bit_position, 7) + 11;
 							for (int i = 0; i < repeat_count; ++i) {
 								literal_lengths.push_back(0);
 							}
@@ -417,7 +455,7 @@ namespace System {
 					DeflateHuffmanTree distance_huffman(distance_huffman_lengths);
 
 					while (true) {
-						int symbol = literal_huffman.decode(data, bit_position);
+						int symbol = literal_huffman.decode(data, data_len, bit_position);
 						if (symbol < 256) {
 							result.push_back(symbol);
 						}
@@ -427,12 +465,12 @@ namespace System {
 						else {
 							int length_extra_bits[] = { 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 0 };
 							int length_starts[] = { 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 15, 17, 19, 23, 27, 31, 35, 43, 51, 59, 67, 83, 99, 115, 131, 163, 195, 227, 258 };
-							int length = length_starts[symbol - 257] + read_bits(data, bit_position, length_extra_bits[symbol - 257]);
+							int length = length_starts[symbol - 257] + read_bits(data, data_len, bit_position, length_extra_bits[symbol - 257]);
 
-							int distance_symbol = distance_huffman.decode(data, bit_position);
+							int distance_symbol = distance_huffman.decode(data, data_len, bit_position);
 							int distance_extra_bits[] = { 0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13 };
 							int distance_starts[] = { 1, 2, 3, 4, 5, 7, 9, 13, 17, 25, 33, 49, 65, 97, 129, 193, 257, 385, 513, 769, 1025, 1537, 2049, 3073, 4097, 6145, 8193, 12289, 16385, 24577 };
-							int distance = distance_starts[distance_symbol] + read_bits(data, bit_position, distance_extra_bits[distance_symbol]);
+							int distance = distance_starts[distance_symbol] + read_bits(data, data_len, bit_position, distance_extra_bits[distance_symbol]);
 
 							int start = result.size() - distance;
 							for (int i = 0; i < length; ++i) {
@@ -441,32 +479,28 @@ namespace System {
 						}
 					}
 				}
-
-
 				// Decompresses data using the Deflate algorithm
-				static std::vector<uint8_t> Decompress(const std::vector<uint8_t>& data) {
+				static std::vector<uint8_t> Decompress(const uint8_t* data, size_t data_len) {
 					std::vector<uint8_t> result;
 					int bit_position = 0;
 
 					while (true) {
-						BlockMarker marker = (BlockMarker)Deflate::read_bits(data, bit_position, 1);
-						BlockType type = (BlockType)Deflate::read_bits(data, bit_position, 2);
+						BlockMarker marker = (BlockMarker)Deflate::read_bits(data, data_len, bit_position, 1);
+						BlockType type = (BlockType)Deflate::read_bits(data, data_len, bit_position, 2);
 
 						if (type == BlockType::Stored) {
-							std::cout << "Stored" << std::endl;
-							ReadStoredBlock(result, data, bit_position);
-							return result;
-						}else if (type == BlockType::Static){
-							std::cout << "Decompress using fixed Huffman codes (pre-agreed Huffman tree defined in the RFC)" << std::endl;
-							ReadStaticBlock(result, data, bit_position);
-							return result;
-						}else if (type == BlockType::Dynamic){
-							std::cout << "Dynamic with their own Huffman trees for literals/lengths and distances" << std::endl;
-							ReadDynamicBlock(result, data, bit_position);
-							return result;
-						}else{
+							ReadStoredBlock(result, data, data_len, bit_position);
+						}
+						else if (type == BlockType::Static) {
+							ReadStaticBlock(result, data, data_len, bit_position);
+						}
+						else if (type == BlockType::Dynamic) {
+							ReadDynamicBlock(result, data, data_len, bit_position);
+						}
+						else {
 							throw std::runtime_error("Error, Invalid Deflate block type");
 						}
+
 						// Check if this is the last block
 						if (marker == BlockMarker::Last) {
 							break;
