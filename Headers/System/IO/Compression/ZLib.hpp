b4e8a45d02324ad983f3558d8a198714a3e2a248
jamie scott <mrjamiescott1995@gmail.com>
2025-08-01 12:26:23 +0100

    Update ZLib.hpp

--- Headers/System/IO/Compression/ZLib.hpp
+++ Headers/System/IO/Compression/ZLib.hpp
@@ -14,13 +14,13 @@ namespace System::IO::Compression {
 
 				struct {
 					// CMF byte
-					uint8_t compressionMethod : 4; // Bits 0â3 (usually 8 for DEFLATE)
-					uint8_t compressionInfo : 4; // Bits 4â7 (e.g. window size)
+					uint8_t compressionMethod : 4; // Bits 0-3 (usually 8 for DEFLATE)
+					uint8_t compressionInfo : 4; // Bits 4-7 (e.g. window size)
 
 					// FLG byte
-					uint8_t fcheck : 5; // Bits 0â4 (header checksum)
+					uint8_t fcheck : 5; // Bits 0-4 (header checksum)
 					uint8_t fdict : 1; // Bit 5 (preset dictionary flag)
-					uint8_t flevel : 2; // Bits 6â7 (compression level)
+					uint8_t flevel : 2; // Bits 6-7 (compression level)
 				};
 			};
 		};
