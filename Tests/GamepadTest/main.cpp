ac0ba426103ce0703581c1f2d1def5bbdb46f7b4
jamie scott <mrjamiescott1995@gmail.com>
2025-08-25 22:41:23 +0100

    Update main.cpp

--- Tests/GamepadTest/main.cpp
+++ Tests/GamepadTest/main.cpp
@@ -6,36 +6,36 @@
 
 
 int main() {
-    GamePad pad;
+    System::Devices::GamePad pad;
 	pad.setDebug(true);
     while (true) {
         pad.update();
 
 
-		if (pad.wasPressedThisFrame(static_cast<int>(GamePad::ButtonCode::BTN_XBOX_A))) {
+		if (pad.wasPressedThisFrame(static_cast<int>(System::Devices::GamePad::ButtonCode::BTN_XBOX_A))) {
 			std::cout << "A button was just pressed\n";
 		}
-		if (pad.wasPressedThisFrame(static_cast<int>(GamePad::ButtonCode::BTN_XBOX_B))) {
+		if (pad.wasPressedThisFrame(static_cast<int>(System::Devices::GamePad::ButtonCode::BTN_XBOX_B))) {
 			std::cout << "B button was just pressed\n";
 		}
-		if (pad.wasPressedThisFrame(static_cast<int>(GamePad::ButtonCode::BTN_XBOX_X))) {
+		if (pad.wasPressedThisFrame(static_cast<int>(System::Devices::GamePad::ButtonCode::BTN_XBOX_X))) {
 			std::cout << "X button was just pressed\n";
 		}
-		if (pad.wasPressedThisFrame(static_cast<int>(GamePad::ButtonCode::BTN_XBOX_Y))) {
+		if (pad.wasPressedThisFrame(static_cast<int>(System::Devices::GamePad::ButtonCode::BTN_XBOX_Y))) {
 			std::cout << "Y button was just pressed\n";
 		}
 		
 		
-		if (pad.wasPressedThisFrame(static_cast<int>(GamePad::ButtonCode::BTN_DPAD_UP))) {
+		if (pad.wasPressedThisFrame(static_cast<int>(System::Devices::GamePad::ButtonCode::BTN_DPAD_UP))) {
 			std::cout << "BTN_DPAD_UP button was just pressed\n";
 		}
-		if (pad.wasPressedThisFrame(static_cast<int>(GamePad::ButtonCode::BTN_DPAD_DOWN))) {
+		if (pad.wasPressedThisFrame(static_cast<int>(System::Devices::GamePad::ButtonCode::BTN_DPAD_DOWN))) {
 			std::cout << "BTN_DPAD_DOWN button was just pressed\n";
 		}
-		if (pad.wasPressedThisFrame(static_cast<int>(GamePad::ButtonCode::BTN_DPAD_LEFT))) {
+		if (pad.wasPressedThisFrame(static_cast<int>(System::Devices::GamePad::ButtonCode::BTN_DPAD_LEFT))) {
 			std::cout << "BTN_DPAD_LEFT button was just pressed\n";
 		}
-		if (pad.wasPressedThisFrame(static_cast<int>(GamePad::ButtonCode::BTN_DPAD_RIGHT))) {
+		if (pad.wasPressedThisFrame(static_cast<int>(System::Devices::GamePad::ButtonCode::BTN_DPAD_RIGHT))) {
 			std::cout << "BTN_DPAD_RIGHT button was just pressed\n";
 		}
 		
