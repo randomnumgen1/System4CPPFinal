8570d74b00611d56092e1ab6eb2a3d7b9a820318
jamie scott <mrjamiescott1995@gmail.com>
2025-08-26 20:08:33 +0100

    Update main.cpp

--- Tests/GamepadTest/main.cpp
+++ Tests/GamepadTest/main.cpp
@@ -25,9 +25,39 @@ int main() {
 			std::cout << "Y button was just pressed\n";
 		}
 		
+		if (pad.wasPressedThisFrame(static_cast<int>(System::Devices::GamePad::ButtonCode::BTN_XBOX_DPAD_UP))) {
+			std::cout << "BTN_XBOX_DPAD_UP was just pressed\n";
+		}
+		if (pad.wasPressedThisFrame(static_cast<int>(System::Devices::GamePad::ButtonCode::BTN_XBOX_DPAD_DOWN))) {
+			std::cout << "BTN_XBOX_DPAD_DOWN was just pressed\n";
+		}
+		if (pad.wasPressedThisFrame(static_cast<int>(System::Devices::GamePad::ButtonCode::BTN_XBOX_DPAD_LEFT))) {
+			std::cout << "BTN_XBOX_DPAD_LEFT was just pressed\n";
+		}
+		if (pad.wasPressedThisFrame(static_cast<int>(System::Devices::GamePad::ButtonCode::BTN_XBOX_DPAD_RIGHT))) {
+			std::cout << "BTN_XBOX_DPAD_RIGHT was just pressed\n";
+		}		
 		
- 
+		if (pad.wasPressedThisFrame(static_cast<int>(System::Devices::GamePad::ButtonCode::BTN_XBOX_STICK_LEFTCLICK))) {
+			std::cout << "BTN_XBOX_STICK_LEFTCLICK was just pressed\n";
+		}
+		if (pad.wasPressedThisFrame(static_cast<int>(System::Devices::GamePad::ButtonCode::BTN_XBOX_STICK_RIGHTCLICK))) {
+			std::cout << "BTN_XBOX_STICK_RIGHTCLICK was just pressed\n";
+		}			
 		
+		if (pad.wasPressedThisFrame(static_cast<int>(System::Devices::GamePad::ButtonCode::BTN_XBOX_LT))) {
+			std::cout << "BTN_XBOX_LT was just pressed\n";
+		}
+		if (pad.wasPressedThisFrame(static_cast<int>(System::Devices::GamePad::ButtonCode::BTN_XBOX_RT))) {
+			std::cout << "BTN_XBOX_RT was just pressed\n";
+		}			
+		
+		if (pad.wasPressedThisFrame(static_cast<int>(System::Devices::GamePad::ButtonCode::BTN_XBOX_LB))) {
+			std::cout << "BTN_XBOX_LB was just pressed\n";
+		}
+		if (pad.wasPressedThisFrame(static_cast<int>(System::Devices::GamePad::ButtonCode::BTN_XBOX_RB))) {
+			std::cout << "BTN_XBOX_RB was just pressed\n";
+		}			
 		
 		
         int lx = pad.getAxis(0);
