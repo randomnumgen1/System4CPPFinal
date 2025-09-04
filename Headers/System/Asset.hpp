8e12d17d720f892615d03a518e7b69ca4e88f844
jamie scott <mrjamiescott1995@gmail.com>
2025-09-04 21:39:17 +0100

    Update Asset.hpp

--- Headers/System/Asset.hpp
+++ Headers/System/Asset.hpp
@@ -10,6 +10,7 @@
 #include <sstream>
 #include <iostream>
 #include <tuple>
+#include <map>
 #include <System/Mesh.h>
 #include <System/Image.hpp>
 #include <System/Vector2.hpp>
@@ -30,6 +31,7 @@ public:
         }
 
         System::Mesh* currentMesh = nullptr;
+        std::map<std::tuple<int, int, int>, int> vertex_map;
 
         std::vector<System::Vector3> temp_vertices;
         std::vector<System::Vector2> temp_uvs;
@@ -41,7 +43,10 @@ public:
             std::string keyword;
             ss >> keyword;
 
-            if (keyword == "o") {
+            if (keyword == "o" || keyword == "g") {
+                if (currentMesh != nullptr) {
+                    vertex_map.clear();
+                }
                 meshes.emplace_back();
                 currentMesh = &meshes.back();
             }
@@ -70,7 +75,8 @@ public:
                     meshes.emplace_back();
                     currentMesh = &meshes.back();
                 }
-                std::vector<std::tuple<int, int, int>> face_indices;
+
+                std::vector<int> face_vertex_indices;
                 std::string face_data;
                 while (ss >> face_data) {
                     int v_idx = 0, vt_idx = 0, vn_idx = 0;
@@ -86,36 +92,55 @@ public:
                     vt_idx = segments.size() > 1 && !segments[1].empty() ? std::stoi(segments[1]) : 0;
                     vn_idx = segments.size() > 2 && !segments[2].empty() ? std::stoi(segments[2]) : 0;
 
-                    face_indices.emplace_back(v_idx, vt_idx, vn_idx);
-                }
+                    std::tuple<int, int, int> face_index_tuple(v_idx, vt_idx, vn_idx);
 
-                if (face_indices.size() < 3) {
-                    // Not a valid face
-                    continue;
-                }
-
-                // Triangulation (fan)
-                for (size_t i = 1; i < face_indices.size() - 1; ++i) {
-                    int indices[3] = { 0, (int)i, (int)i + 1 };
-
-                    for (int j = 0; j < 3; ++j) {
-                        auto& face_index = face_indices[indices[j]];
-                        int v_idx = std::get<0>(face_index);
-                        int vt_idx = std::get<1>(face_index);
-                        int vn_idx = std::get<2>(face_index);
-
+                    auto it = vertex_map.find(face_index_tuple);
+                    if (it != vertex_map.end()) {
+                        face_vertex_indices.push_back(it->second);
+                    }
+                    else {
                         // Handle negative indices
                         if (v_idx < 0) v_idx = temp_vertices.size() + v_idx + 1;
                         if (vt_idx < 0) vt_idx = temp_uvs.size() + vt_idx + 1;
                         if (vn_idx < 0) vn_idx = temp_normals.size() + vn_idx + 1;
 
-                        if (v_idx > 0) {
+                        if (v_idx > 0 && (size_t)v_idx <= temp_vertices.size()) {
                             currentMesh->vertices.push_back(temp_vertices[v_idx - 1]);
-                            currentMesh->triangles.push_back(currentMesh->vertices.size() - 1);
                         }
-                        if (vt_idx > 0) currentMesh->uvs.push_back(temp_uvs[vt_idx - 1]);
-                        if (vn_idx > 0) currentMesh->normals.push_back(temp_normals[vn_idx - 1]);
+                        else {
+                            currentMesh->vertices.push_back(System::Vector3(0, 0, 0));
+                        }
+
+                        if (vt_idx > 0 && (size_t)vt_idx <= temp_uvs.size()) {
+                            currentMesh->uvs.push_back(temp_uvs[vt_idx - 1]);
+                        }
+                        else {
+                            currentMesh->uvs.push_back(System::Vector2(0, 0));
+                        }
+
+                        if (vn_idx > 0 && (size_t)vn_idx <= temp_normals.size()) {
+                            currentMesh->normals.push_back(temp_normals[vn_idx - 1]);
+                        }
+                        else {
+                            currentMesh->normals.push_back(System::Vector3(0, 0, 0));
+                        }
+
+                        int new_vertex_index = currentMesh->vertices.size() - 1;
+                        vertex_map[face_index_tuple] = new_vertex_index;
+                        face_vertex_indices.push_back(new_vertex_index);
                     }
+                }
+
+                if (face_vertex_indices.size() < 3) {
+                    // Not a valid face
+                    continue;
+                }
+
+                // Triangulation (fan)
+                for (size_t i = 1; i < face_vertex_indices.size() - 1; ++i) {
+                    currentMesh->triangles.push_back(face_vertex_indices[0]);
+                    currentMesh->triangles.push_back(face_vertex_indices[i]);
+                    currentMesh->triangles.push_back(face_vertex_indices[i + 1]);
                 }
             }
         }
