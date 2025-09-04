88bf21c44f54e1fe942e4a62da3c07a4d9f2c0df
jamie scott <mrjamiescott1995@gmail.com>
2025-09-04 20:19:17 +0100

    Create Asset.hpp

---
+++ Headers/System/Asset.hpp
@@ -0,0 +1,30 @@
+
+#ifndef _SYSTEM_ASSET_H
+#define _SYSTEM_ASSET_H
+
+#include <string>
+
+class Asset {
+public:
+    void LoadOBJ(const std::string& filepath) {
+
+    }
+    void LoadDAE(const std::string& filepath) {
+
+    }
+    void LoadFBX(const std::string& filepath) {
+
+    }
+    void LoadGLB(const std::string& filepath) {
+
+    }
+    void LoadGLTF(const std::string& filepath) {
+
+    }
+ 
+
+
+
+
+};
+#endif
