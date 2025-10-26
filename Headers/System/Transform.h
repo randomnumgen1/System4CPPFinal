1593b52c738d7774af7ab9c23ec8532bd6f10b90
jamie scott <mrjamiescott1995@gmail.com>
2025-10-26 19:03:58 +0000

    Update Transform.h

--- Headers/System/Transform.h
+++ Headers/System/Transform.h
@@ -1,34 +1,86 @@
-#pragma once
+/*
+SetParent works by modifying the Transform hierarchy, not the GameObject itself. 
+Since every GameObject has a Transform component, parenting is handled at the Transform level rather than the GameObject level.
+
+*/
+#ifndef _SYSTEM_TRANSFORM_H
+#define _SYSTEM_TRANSFORM_H
+
+
 #include <iostream>
-#include <System/Vector3.h>
-#include <System/Quaternion.h>
-#include "System/Matrix4x4.h"
+#include <System/Vector3.hpp>
+#include <System/Quaternion.hpp>
+#include <System/Matrix4x4.hpp>
 enum Space { Self, World };
+namespace System{
 class Transform {
+private:
+
 public:
+    /// <summary>
+    /// The position of the transform in world space.
+    /// </summary>
     System::Vector3 position;
+    /// <summary>
+    /// The rotation of the transform in world space.
+    /// </summary>
     System::Quaternion rotation;
+    /// <summary>
+    /// The scale of the transform in world space.
+    /// </summary>
     System::Vector3 scale;
 
+
+    System::Vector3 localPosition;
+    System::Quaternion localRotation;
+    System::Vector3 localScale;
+
+
+
+    Transform *parent;
+    bool hasChanged;
     Transform(){
         position = System::Vector3(0.0f, 0.0f, 0.0f);
         rotation = System::Quaternion::Euler(0.0f, 0.0f, 0.0f);
         scale = System::Vector3(1.0f, 1.0f, 1.0f);
     }
+    System::Vector3 right() const {
+        return rotation * Vector3::right;
+    }
+    System::Vector3 up() const {
+        return rotation * Vector3::up;
+    }
+    System::Vector3 forward() const {
+        return rotation * Vector3::forward;
+    }
+    void SetParent(Transform* p) {
+
+    }
+    void SetParent(Transform* p, bool worldPositionStays) {
+    
+    }
+
+
     void Rotate(System::Vector3 eulers, Space relativeTo) {
     
     }
     void Rotate(float xAngle, float yAngle, float zAngle, Space relativeTo) {
     
     }
-    System::Matrix4x4  GetLocalToWorldMatrix() const {
-        return System::Matrix4x4::Translation(position) *  System::Matrix4x4::Rotation(rotation) *  System::Matrix4x4::Scaling(scale);
-
-
+    void Rotate(System::Vector3 eulers, Space relativeTo) {
+        System::Quaternion rotationDelta = System::Quaternion::Euler(eulers.x, eulers.y, eulers.z);
+        if (relativeTo == Self) {
+            rotation = rotation * rotationDelta; // Local rotation
+        }
+        else {
+            rotation = rotationDelta * rotation; // World rotation
+        }
+    }
+    System::Matrix4x4 GetLocalToWorldMatrix() const {
         System::Matrix4x4 localMatrix = System::Matrix4x4::Translation(position) * System::Matrix4x4::Rotation(rotation) * System::Matrix4x4::Scaling(scale);
-      //  if (parent) {
-       //     return parent->GetLocalToWorldMatrix() * localMatrix;
-      //  }
+        if (parent) {
+            return parent->GetLocalToWorldMatrix() * localMatrix;
+        }
         return localMatrix;
     }
     void SetPosition(float nx, float ny, float nz) {
@@ -36,8 +88,12 @@ public:
         position.x = nx;
         position.y = ny;
         position.z = nz;
+        hasChanged = true;
     }
     void PrintPosition() const {
         std::cout << "Position: (" << position.x << ", " << position.y << ", " << position.z << ")" << std::endl;
     }
+
 };
+}
+#endif
