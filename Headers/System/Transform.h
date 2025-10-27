c6ae302d04d7540a20a6c7489934cded645af460
jamie scott <mrjamiescott1995@gmail.com>
2025-10-27 17:16:36 +0000

    Update Transform.h

--- Headers/System/Transform.h
+++ Headers/System/Transform.h
@@ -12,6 +12,7 @@ Since every GameObject has a Transform component, parenting is handled at the Tr
 #include <System/Quaternion.hpp>
 #include <System/Matrix4x4.hpp>
 #include <System/Component.h>
+#include <vector>
 enum Space { Self, World };
 namespace System{
 class Transform : System::Component {
