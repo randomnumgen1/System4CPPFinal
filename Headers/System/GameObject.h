f307a8671afbb68e815cbf7de702ed7128cfc878
jamie scott <mrjamiescott1995@gmail.com>
2025-10-27 15:46:45 +0000

    Create GameObject.h

---
+++ Headers/System/GameObject.h
@@ -0,0 +1,65 @@
+#ifndef _SYSTEM_GAMEOBJECT_H
+#define _SYSTEM_GAMEOBJECT_H
+#include <unordered_map>
+#include <typeindex>
+#include <string>
+#include "Transform.h"
+namespace System {
+    enum class PrimitiveType { 
+        Sphere,
+        Capsule,
+        Cylinder,
+        Cube,
+        Plane,
+        Quad
+    };
+    struct GameObject {
+        std::string name;
+
+        std::shared_ptr<Transform> transform; 
+
+        GameObject(const std::string& name, System::Transform* parent = nullptr)
+            : name(name), transform(std::make_shared<Transform>(parent)) {}
+
+
+
+        static GameObject CreatePrimitive(PrimitiveType type){
+        
+        
+        }
+        static GameObject Instantiate(GameObject original, Vector3 position, Quaternion rotation){
+        
+        }
+
+
+
+
+
+
+
+
+
+
+
+        std::unordered_map<std::type_index, void*> components;
+        template<typename T>
+        void AddComponent(T* component) {
+            components[std::type_index(typeid(T))] = component;
+        }
+        template<typename T>
+        T* GetComponent() {
+            auto it = components.find(std::type_index(typeid(T)));
+            if (it != components.end()) {
+                return static_cast<T*>(it->second);
+            }
+            return nullptr;
+        }
+
+        template<typename T>
+        bool TryGetComponent(T*& component) {
+            component = GetComponent<T>();
+            return component != nullptr;
+        }
+    };
+}
+#endif
