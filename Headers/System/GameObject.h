fc683faa4ba58bf6f78d33fb3546e102595fd00c
jamie scott <mrjamiescott1995@gmail.com>
2025-10-28 21:25:24 +0000

    Update GameObject.h

--- Headers/System/GameObject.h
+++ Headers/System/GameObject.h
@@ -21,6 +21,7 @@ namespace System {
         GameObject(const std::string& name) : name(name) {
             transform = new Transform();
             transform->m_gameObject = this;
+            //automatically parent new objects to the scene root.
             if (Scene::root && this != Scene::root) {
                 transform->SetParent(Scene::root->transform);
             }
