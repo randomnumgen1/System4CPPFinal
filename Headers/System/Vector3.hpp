7ce557bd88c102d37fc3a28638771cb929fd5809
jamie scott <mrjamiescott1995@gmail.com>
2025-10-30 20:44:10 +0000

    Vector3: implement missing times and divide

--- Headers/System/Vector3.hpp
+++ Headers/System/Vector3.hpp
@@ -110,6 +110,20 @@ namespace System {
                 z * scalar
             };
         }
+        Vector3 operator*(const Vector3& other) const {
+            return Vector3{
+                x * other.x,
+                y * other.y,
+                z * other.z
+            };
+        }
+        Vector3 operator/(const Vector3& other) const {
+            return Vector3{
+                x / other.x,
+                y / other.y,
+                z / other.z
+            };
+        }
         Vector3 operator-(const Vector3& other) const {
             return Vector3{
                 x - other.x,
