a5f4f370d7d9f3b9c9fdec2a1dcae3af486a11de
jamie scott <mrjamiescott1995@gmail.com>
2025-10-30 20:18:56 +0000

    Update Light.h

--- Headers/System/Light.h
+++ Headers/System/Light.h
@@ -8,6 +8,7 @@
 #include <System/Quaternion.hpp>
 #include <System/Matrix4x4.hpp>
 #include <System/Component.h>
+#include "Color32.hpp"
 
 namespace System {
     enum LightType {
@@ -20,6 +21,10 @@ namespace System {
         
     public:
         LightType type;
+        float range;
+        Color32 color;
+        float intensity;
+
     };
 }
 #endif
