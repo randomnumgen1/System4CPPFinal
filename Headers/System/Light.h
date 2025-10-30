68b32b6e880efd1b87b38703b0435047c59386e9
jamie scott <mrjamiescott1995@gmail.com>
2025-10-30 20:12:06 +0000

    Create Light.h

---
+++ Headers/System/Light.h
@@ -0,0 +1,25 @@
+#ifndef _SYSTEM_LIGHT_H
+#define _SYSTEM_LIGHT_H
+
+
+
+#include <iostream>
+#include <System/Vector3.hpp>
+#include <System/Quaternion.hpp>
+#include <System/Matrix4x4.hpp>
+#include <System/Component.h>
+
+namespace System {
+    enum LightType {
+        Spot,
+        Directional,
+        Point
+    };
+    class Light : public System::Component {
+    private:
+        
+    public:
+        LightType type;
+    };
+}
+#endif
