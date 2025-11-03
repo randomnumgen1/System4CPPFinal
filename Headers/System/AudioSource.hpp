4121f2767a76926a5f1fd8d0c011c3d94896b340
jamie scott <mrjamiescott1995@gmail.com>
2025-11-03 19:15:55 +0000

    Create AudioSource.hpp

---
+++ Headers/System/AudioSource.hpp
@@ -0,0 +1,55 @@
+#ifndef _SYSTEM_AUDIOSOURCE_H
+#define _SYSTEM_AUDIOSOURCE_H
+
+#include <System/Component.hpp>
+#include "AudioClip.hpp"
+#include "Vector3.hpp"
+
+namespace System {
+    /// <summary>
+    /// usually attached to a gameobject to play sounds from that position.
+    /// </summary>
+    struct AudioSource : public System::Component {
+        System::AudioClip* m_clip;
+        float m_volume;
+        bool m_loop;
+        void SetClip(System::AudioClip* clip) {
+            m_clip = clip;
+        }
+        void SetVolume(float volume) {
+            m_volume = volume;
+        }
+        void SetLoop(bool loop) {
+            m_loop = loop;
+        }
+        void PlayOneShot(AudioClip* clip) {
+            PlayOneShot(clip,  1.0f);
+        }
+        void PlayOneShot(AudioClip* clip, float volumeScale) {
+        
+        }
+        void Play(){
+        
+        }
+        void Pause(){
+        
+        }
+        void Stop() {
+        
+        }
+        Component* Clone() const override {
+            AudioSource* newSource = new AudioSource(*this);
+            newSource->m_clip = m_clip;
+            return newSource;
+        }
+        static void PlayClipAtPoint(AudioClip* clip, System::Vector3 position) {
+            PlayClipAtPoint(clip, position, 1.0f);
+        }
+        static void PlayClipAtPoint(AudioClip* clip, System::Vector3 position, float volume) {
+        
+        }
+
+    };
+}
+
+#endif
