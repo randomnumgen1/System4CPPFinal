6b127f15ad95ccc4b749f54c5a066d0518b86a61
jamie scott <mrjamiescott1995@gmail.com>
2025-11-03 20:30:03 +0000

    Create AudioClip.hpp

---
+++ Headers/System/AudioClip.hpp
@@ -0,0 +1,17 @@
+#ifndef _SYSTEM_AUDIOCLIP_H
+#define _SYSTEM_AUDIOCLIP_H
+
+
+
+namespace System {
+    /// <summary>
+    /// 
+    /// </summary>
+    struct AudioClip {
+
+
+
+    };
+}
+
+#endif
