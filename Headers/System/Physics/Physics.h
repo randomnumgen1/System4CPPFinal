0dfafbbccc46d5a0fed4a1f3bababbaca64fe746
jamie scott <mrjamiescott1995@gmail.com>
2025-11-10 18:40:54 +0000

    Physics

--- Headers/System/Physics/Physics.h
+++ Headers/System/Physics/Physics.h
@@ -4,19 +4,9 @@
 #include <System/Vector3.hpp>
 #include <cstdint>
 #include <System/Mathf.hpp>
-#include <memory>
 #include <System/Mesh.hpp>
 
-
-#if defined(SYSTEM_PHYSICS_BULLET)
-class btDefaultCollisionConfiguration;
-class btCollisionDispatcher;
-class btBroadphaseInterface;
-class btSequentialImpulseConstraintSolver;
-class btDiscreteDynamicsWorld;
-#endif
-
-namespace System{
+namespace System {
     struct Ray {
     public:
         Vector3 origin;
@@ -24,48 +14,21 @@ namespace System{
         Ray(const Vector3& origin, const Vector3& direction)
             : origin(origin), direction(direction) {}
     };
+
     struct RaycastHit {
     public:
         Vector3 point;
         Vector3 normal;
         float distance;
     };
-    class Physics{
-    private:
-        //TODO: add a friend class for allowing access to private members e.g rigid body
-#if defined(SYSTEM_PHYSICS_BULLET)
-        static std::unique_ptr<btDefaultCollisionConfiguration> collisionConfiguration;
-        static std::unique_ptr<btCollisionDispatcher> dispatcher;
-        static std::unique_ptr<btBroadphaseInterface> broadphase;
-        static std::unique_ptr<btSequentialImpulseConstraintSolver> solver;
-        static std::unique_ptr<btDiscreteDynamicsWorld> dynamicsWorld;
-#endif
+
+    class Physics {
     public:
-        static void Initialize();
-        static void Shutdown();
         static bool Raycast(Vector3 origin, Vector3 direction, float maxDistance = System::Mathf::Infinity, uint32_t layerMask = -1);
         static bool Raycast(Vector3 origin, Vector3 direction, RaycastHit& hitInfo, float maxDistance, uint32_t layerMask);
         static void Simulate(float step);
         static void Setgravity(const Vector3& gravity);
-
-
-        static void GenerateCollider(const System::Mesh& mesh) {
-#if defined(SYSTEM_PHYSICS_BULLET)
-            btConvexHullComputer ch;
-            ch.compute(&mesh.vertices[0].x, sizeof(System::Vector3), mesh.vertices.size(), 0.0f, 0.0f);
-
-            // Access hull points:
-            for (int i = 0; i < ch.vertices.size(); ++i) {
-                btVector3 pt(ch.vertices[i]);
-                // Use pt to build your new mesh
-            }
-#endif
-        }
-
-
-
-
-
+        static void GenerateCollider(const System::Mesh& mesh);
     };
 }
 #endif
--- Sources/System/Physics/Physics.cpp
+++ Sources/System/Physics/Physics.cpp
@@ -1,111 +1,25 @@
 #include <System/Physics/Physics.h>
-
-#if defined(SYSTEM_PHYSICS_BULLET)
-#include <bullet/btBulletDynamicsCommon.h>
-#endif
-
+#include <System/Scene.hpp>
+#include <System/Internal/Physics/PhysicsEngine.hpp>
 
 namespace System {
-    void Physics::Initialize(){
-#if defined(SYSTEM_PHYSICS_BULLET)
-        collisionConfiguration = std::make_unique<btDefaultCollisionConfiguration>();
-        dispatcher = std::make_unique<btCollisionDispatcher>(collisionConfiguration.get());
-        broadphase = std::make_unique<btDbvtBroadphase>();
-        solver = std::make_unique<btSequentialImpulseConstraintSolver>();
-        dynamicsWorld = std::make_unique<btDiscreteDynamicsWorld>(dispatcher.get(), broadphase.get(), solver.get(), collisionConfiguration.get());
-        dynamicsWorld->setGravity(btVector3(0, -9.81f, 0));
-#endif
-    }
-    void Physics::Shutdown() {
-#if defined(SYSTEM_PHYSICS_BULLET)
-        dynamicsWorld.reset();
-        solver.reset();
-        broadphase.reset();
-        dispatcher.reset();
-        collisionConfiguration.reset();
-#endif
+    bool Physics::Raycast(Vector3 origin, Vector3 direction, float maxDistance, uint32_t layerMask) {
+        return Scene::physicsEngine->Raycast(origin, direction, maxDistance, layerMask);
     }
 
-
-
-
-
-
-
-    bool Physics::Raycast(Vector3 origin, Vector3 direction, float maxDistance, uint32_t layerMask){
-#if defined(SYSTEM_PHYSICS_BULLET)
-        if (!dynamicsWorld) return false;
-
-        btVector3 btOrigin(origin.x, origin.y, origin.z);
-        btVector3 btDirection(direction.x, direction.y, direction.z);
-        btVector3 btEnd = btOrigin + btDirection.normalized() * maxDistance;
-
-        btCollisionWorld::ClosestRayResultCallback rayCallback(btOrigin, btEnd);
-        dynamicsWorld->rayTest(btOrigin, btEnd, rayCallback);
-        return rayCallback.hasHit();
-#else
-        return false;
-#endif
+    bool Physics::Raycast(Vector3 origin, Vector3 direction, RaycastHit& hitInfo, float maxDistance, uint32_t layerMask) {
+        return Scene::physicsEngine->Raycast(origin, direction, hitInfo, maxDistance, layerMask);
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    bool Physics::Raycast(Vector3 origin, Vector3 direction, RaycastHit& hitInfo, float maxDistance, uint32_t layerMask){
-#if defined(SYSTEM_PHYSICS_BULLET)
-        if (!dynamicsWorld) return false;
-
-        btVector3 btOrigin(origin.x, origin.y, origin.z);
-        btVector3 btDirection(direction.x, direction.y, direction.z);
-        btVector3 btEnd = btOrigin + btDirection.normalized() * maxDistance;
-
-        btCollisionWorld::ClosestRayResultCallback rayCallback(btOrigin, btEnd);
-        dynamicsWorld->rayTest(btOrigin, btEnd, rayCallback);
-        if (rayCallback.hasHit()) {
-            hitInfo.point = Vector3(rayCallback.m_hitPointWorld.x(), rayCallback.m_hitPointWorld.y(), rayCallback.m_hitPointWorld.z());
-            hitInfo.normal = Vector3(rayCallback.m_hitNormalWorld.x(), rayCallback.m_hitNormalWorld.y(), rayCallback.m_hitNormalWorld.z());
-            hitInfo.distance = (hitInfo.point - origin).magnitude();
-            return true;
-        }
-#endif
-        return false;
+    void Physics::Simulate(float step) {
+        Scene::physicsEngine->Simulate(step);
     }
 
-
-
-
-
-
-
-
-
-
-    void Physics::Simulate(float step){
-#if defined(SYSTEM_PHYSICS_BULLET)
-    dynamicsWorld->stepSimulation(step);
-#endif
-}
-
-    void Physics::Setgravity(const Vector3& gravity){
-#if defined(SYSTEM_PHYSICS_BULLET)
-        dynamicsWorld->setGravity(btVector3(gravity.x, gravity.y, gravity.z));
-#endif
+    void Physics::Setgravity(const Vector3& gravity) {
+        Scene::physicsEngine->Setgravity(gravity);
     }
 
-
+    void Physics::GenerateCollider(const System::Mesh& mesh) {
+        Scene::physicsEngine->GenerateCollider(mesh);
+    }
 }
