694dbdb2a1ba4a85e5e316c9a4c555b3af8904ea
jamie scott <mrjamiescott1995@gmail.com>
2025-11-11 18:11:49 +0000

    BoxCollider

--- Headers/System/BoxCollider.hpp
+++ Headers/System/BoxCollider.hpp
@@ -9,6 +9,9 @@ namespace System {
     public:
         Vector3 center;
         Vector3 size;
+
+        Vector3 ClosestPoint(Vector3 position) override;
+        Vector3 ClosestPointOnBounds(Vector3 position) override;
     };
 }
 #endif
---
+++ Sources/System/BoxCollider.cpp
@@ -0,0 +1,15 @@
+#include <System/Vector3.hpp>
+#include "System/BoxCollider.hpp"
+
+namespace System {
+
+    Vector3 BoxCollider::ClosestPoint(Vector3 position) {
+        // TODO: Implement ClosestPoint
+        return Vector3(0, 0, 0);
+    }
+
+    Vector3 BoxCollider::ClosestPointOnBounds(Vector3 position) {
+        // TODO: Implement ClosestPointOnBounds
+        return Vector3(0, 0, 0);
+    } 
+}
