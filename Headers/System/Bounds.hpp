a835e374bf346cb0003f68965a9b0001b3967f5a
jamie scott <mrjamiescott1995@gmail.com>
2025-11-16 10:23:53 +0000

    Bounds split into h and cpp

--- Headers/System/Bounds.hpp
+++ Headers/System/Bounds.hpp
@@ -30,6 +30,12 @@ namespace System {
             float dz = std::max(min.z - point.z, 0.0f) + std::max(point.z - max.z, 0.0f);
             return dx * dx + dy * dy + dz * dz;
         }
+        Vector3 ClosestPoint(const Vector3 point) const {
+            float x = std::max(min.x, std::min(point.x, max.x));
+            float y = std::max(min.y, std::min(point.y, max.y));
+            float z = std::max(min.z, std::min(point.z, max.z));
+            return Vector3(x, y, z);
+        }
     };
 }
 
---
+++ Sources/System/Bounds.cpp
@@ -0,0 +1,3 @@
+#include <System/Bounds.hpp>
+
+
