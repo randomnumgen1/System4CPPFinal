a52076656b6e94e9733cc973b281496eac82ca5e
jamie scott <mrjamiescott1995@gmail.com>
2025-11-23 19:51:39 +0000

    Update DummyPhysicsEngine.cpp

---
+++ Sources/System/Internal/Physics/DummyPhysicsEngine.cpp
@@ -0,0 +1,31 @@
+#include <System/Internal/Physics/DummyPhysicsEngine.hpp>
+
+
+
+void System::DummyPhysicsEngine::Initialize(){
+    std::cout << "DummyPhysicsEngine::Initialize" << std::endl;
+}
+
+void System::DummyPhysicsEngine::Shutdown(){
+    std::cout << "DummyPhysicsEngine::Shutdown" << std::endl;
+}
+
+bool System::DummyPhysicsEngine::Raycast(Vector3 origin, Vector3 direction, float maxDistance, uint32_t layerMask){
+    return false;
+}
+
+bool System::DummyPhysicsEngine::Raycast(Vector3 origin, Vector3 direction, RaycastHit& hitInfo, float maxDistance, uint32_t layerMask){
+    return false;
+}
+
+void System::DummyPhysicsEngine::Simulate(float step){
+    std::cout << "DummyPhysicsEngine::Simulate" << std::endl;
+}
+
+void System::DummyPhysicsEngine::Setgravity(const Vector3& gravity){
+    std::cout << "DummyPhysicsEngine::Setgravity" << std::endl;
+}
+
+void System::DummyPhysicsEngine::GenerateCollider(const System::Mesh& mesh){
+
+}
