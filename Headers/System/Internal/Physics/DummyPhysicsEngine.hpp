1ede5cb41104fc6bdc989ac943c8cd1610fffc00
jamie scott <mrjamiescott1995@gmail.com>
2025-11-23 19:46:45 +0000

    Update DummyPhysicsEngine.hpp

---
+++ Headers/System/Internal/Physics/DummyPhysicsEngine.hpp
@@ -0,0 +1,24 @@
+#ifndef _SYSTEM_BULLET_PHYSICS_ENGINE_HPP
+#define _SYSTEM_BULLET_PHYSICS_ENGINE_HPP
+
+#include "PhysicsEngineInterface.hpp"
+#include <memory>
+
+
+namespace System {
+    class DummyPhysicsEngine : public PhysicsEngineInterface {
+    private:
+
+
+    public:
+        void Initialize() override;
+        void Shutdown() override;
+        bool Raycast(Vector3 origin, Vector3 direction, float maxDistance, uint32_t layerMask) override;
+        bool Raycast(Vector3 origin, Vector3 direction, RaycastHit& hitInfo, float maxDistance, uint32_t layerMask) override;
+        void Simulate(float step) override;
+        void Setgravity(const Vector3& gravity) override;
+        void GenerateCollider(const System::Mesh& mesh) override;
+    };
+}
+
+#endif
