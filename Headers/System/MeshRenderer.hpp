1df61184fe0ca7d4afa786292076cbdfdc496c20
jamie scott <mrjamiescott1995@gmail.com>
2025-11-25 19:55:17 +0000

    Update MeshRenderer.hpp

--- Headers/System/MeshRenderer.hpp
+++ Headers/System/MeshRenderer.hpp
@@ -32,9 +32,8 @@ namespace System {
             meshFilter->mesh->UploadMeshData(false);
 
             System::Graphics::GL::gl_glBindVertexArray(meshFilter->mesh->GetVAO());
-            System::Graphics::GL::gl_glDrawElements(GL_TRIANGLES, meshFilter->mesh->indices.size(), GL_UNSIGNED_INT, 0);//fix
-            System::Graphics::GL::gl_glBindVertexArray(0);
-             
+            System::Graphics::GL::gl_glDrawElements(System::Graphics::DrawMode::TRIANGLES, meshFilter->mesh->indices.size(), System::Graphics::IndexType::UNSIGNED_INT, 0);//fix
+            System::Graphics::GL::gl_glBindVertexArray(0); 
         }
         Component* Clone() const override {
             MeshRenderer* new_renderer = new MeshRenderer();
