e04f8631d170f397bee051021049ec1896f52978
jamie scott <mrjamiescott1995@gmail.com>
2025-12-22 19:44:14 +0000

    FreeCam: cleanup

--- Headers/System/Components/freecam.h
+++ Headers/System/Components/freecam.h
@@ -29,33 +29,23 @@ namespace System {
         float speedH = 2.0f;
         float speedV = 2.0f;
     
-        FreeCam()
-        {
-           
+        FreeCam(){
+            camera = nullptr;
 
         }
+
         void Update() override {
+            if (camera == nullptr) {
+                return;
+            }
             bool fastMode = System::Input::GetKey(System::KeyCode::LeftShift);
             float movementSpeed = fastMode ? 100.0f : 10.0f;
-        
-            if (System::Input::GetKey(System::KeyCode::W) || Input::GetKey(System::KeyCode::UpArrow)) {
-                System::Vector3 forward = camera->transform()->forward();
-                camera->transform()->SetPosition(camera->transform()->GetPosition() + (camera->transform()->forward() * movementSpeed * System::Time::deltaTime));
-            }
-            if (System::Input::GetKey(System::KeyCode::S) || Input::GetKey(System::KeyCode::DownArrow))  {
-                System::Vector3 forward = camera->transform()->forward();
-                camera->transform()->SetPosition(camera->transform()->GetPosition() + (-camera->transform()->forward() * movementSpeed * System::Time::deltaTime));
-            }
-            if (System::Input::GetKey(System::KeyCode::A) || Input::GetKey(System::KeyCode::LeftArrow)) {
-                System::Vector3 right = camera->transform()->right();
-                camera->transform()->SetPosition(camera->transform()->GetPosition() + (-camera->transform()->right() * movementSpeed * System::Time::deltaTime));
-            }
-            if (System::Input::GetKey(System::KeyCode::D) || Input::GetKey(System::KeyCode::RightArrow)) {
-                System::Vector3 right = camera->transform()->right();
-                camera->transform()->SetPosition(camera->transform()->GetPosition() + (camera->transform()->right() * movementSpeed * System::Time::deltaTime));
-            }
 
-          
+
+            
+            //rotation
+
+
             yaw += speedH * System::Input::GetAxis("MouseX");
             pitch += speedV * System::Input::GetAxis("MouseY");
             if (pitch > 89.0f) pitch = 89.0f;
@@ -64,6 +54,75 @@ namespace System {
 
             
             camera->transform()->seteulerAngles(System::Vector3(pitch, yaw, 0.0f)); 
+
+
+            //movement
+
+
+            System::Vector3 move = System::Vector3::zero;
+
+            // local movement
+
+
+            // Move the camera forward based on its current facing direction.
+            if (System::Input::GetKey(System::KeyCode::W)) {
+                move += camera->transform()->forward();
+            }
+            // Move the camera backward opposite to its current facing direction.
+            if (System::Input::GetKey(System::KeyCode::S)) {
+                move -= camera->transform()->forward();
+            }
+            // Move the camera left relative to its current rotation.
+            if (System::Input::GetKey(System::KeyCode::A)) {
+                move -= camera->transform()->right();
+            }
+            // Move the camera right relative to its current rotation.
+            if (System::Input::GetKey(System::KeyCode::D)) {
+                move += camera->transform()->right();
+            }
+
+
+            // world movement
+
+            // Move the camera forward in world space (ignores camera rotation)
+            if (System::Input::GetKey(System::KeyCode::I)) {
+                move += Vector3::forward;
+            }
+            // Move the camera backward in world space (ignores camera rotation)
+            if (System::Input::GetKey(System::KeyCode::K)) {
+                move += Vector3::back;
+            }
+            // Move the camera left in world space (ignores camera rotation)
+            if (System::Input::GetKey(System::KeyCode::J)) {
+                move += Vector3::left;
+            }
+            // Move the camera right in world space (ignores camera rotation)
+            if (System::Input::GetKey(System::KeyCode::L)) {
+                move += Vector3::right;
+            }
+
+
+            // Move the camera up in world space (ignores camera rotation)
+            if (System::Input::GetKey(System::KeyCode::N)) {
+                move += Vector3::up;
+            }
+            // Move the camera down in world space (ignores camera rotation)
+            if (System::Input::GetKey(System::KeyCode::M)) {
+                move += Vector3::down;
+            }
+
+            if (move != Vector3::zero) {
+                camera->transform()->SetPosition(camera->transform()->GetPosition() + move.normalized() * movementSpeed * System::Time::deltaTime);
+            }
+
+
+
+
+             
+
+
+          
+
              
 
 
