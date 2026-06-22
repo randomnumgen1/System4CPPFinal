16f4b20bca32b512c08e0256738153ec6c08ec83
jamie scott <mrjamiescott1995@gmail.com>
2026-06-22 21:50:36 +0100

    Update RenderTexture.h

--- Headers/System/RenderTexture.h
+++ Headers/System/RenderTexture.h
@@ -5,18 +5,25 @@
 #include <System/Graphics/GraphicsHelpers.h>
 typedef int* IntPtr_t;
 namespace System {
+    enum class RenderTextureType : uint32_t {
+        Default = 0
+    };
+    //RenderTextureType
     struct RenderTexture {
         uint32_t renderedTexture;
         int m_width;
         int m_height;
         
-        RenderTexture(int width, int height, int depth) : m_width(width), m_height(height) {
-            renderedTexture = 0;
+    
+        RenderTexture(int width, int height, int depth)
+            : RenderTexture(width, height, depth, RenderTextureType::Default) {}
+
+        RenderTexture(int width, int height, int depth, RenderTextureType RenderType)
+            : renderedTexture(0), m_width(width), m_height(height) {
+
             System::Graphics::GL::gl_glGenTextures(1, &renderedTexture);
             System::Graphics::GL::gl_glBindTexture(System::Graphics::GLenum1::GL_TEXTURE_2D, renderedTexture);
-           // System::Graphics::GL::gl_glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
-
-
+            // System::Graphics::GL::gl_glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
         }
         bool isCreated() const {
             return renderedTexture != 0;
