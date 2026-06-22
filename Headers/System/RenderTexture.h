60116974a4e3c59b1de94bc48063b23b8f17a958
jamie scott <mrjamiescott1995@gmail.com>
2026-06-22 22:03:18 +0100

    Update RenderTexture.h

--- Headers/System/RenderTexture.h
+++ Headers/System/RenderTexture.h
@@ -6,7 +6,12 @@
 typedef int* IntPtr_t;
 namespace System {
     enum class RenderTextureType : uint32_t {
-        Default = 0
+        Default = 0,
+        /// <summary>
+        /// 8 bits per channel
+        /// </summary>
+        ARGB32,
+        BGRA32
     };
     //RenderTextureType
     struct RenderTexture {
@@ -20,7 +25,9 @@ namespace System {
 
         RenderTexture(int width, int height, int depth, RenderTextureType RenderType)
             : renderedTexture(0), m_width(width), m_height(height) {
-
+            if (RenderType == RenderTextureType::Default) {
+                throw std::exception("RenderTexture: default not supported yet");
+            }
             System::Graphics::GL::gl_glGenTextures(1, &renderedTexture);
             System::Graphics::GL::gl_glBindTexture(System::Graphics::GLenum1::GL_TEXTURE_2D, renderedTexture);
             // System::Graphics::GL::gl_glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
